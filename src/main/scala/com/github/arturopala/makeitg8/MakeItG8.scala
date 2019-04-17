0f467bd8cd50b09db7c855c04a49ac3eca189d87
Artur Opala <250927+arturopala@users.noreply.github.com>
2019-04-17 12:26:33 +0100

    handle scallop errors

--- src/main/scala/com/github/arturopala/makeitg8/MakeItG8.scala
+++ src/main/scala/com/github/arturopala/makeitg8/MakeItG8.scala
@@ -32,19 +32,14 @@ object MakeItG8 extends App with MakeItG8Creator {
     _ => {
       println()
       println("Sorry, your command is missing something, consult the doc and try again!")
-      System.exit(-1)
     },
     config =>
       createG8Template(config).fold(
         _ => {
           println()
           println("Sorry, something went wrong, check the log and try again!")
-          System.exit(-1)
         },
-        _ => {
-          println("Done.")
-          System.exit(0)
-        }
+        _ => println("Done.")
     )
   )
 
